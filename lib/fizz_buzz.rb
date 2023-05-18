1a60c819eb40c3c85f8dfd4bb72f4590cedb8da2
Hayato Koshi <kshyt2501@icloud.com>
2023-05-18 18:15:51 +0900

    first commit

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,24 @@
+def fizz_buzz(target)
+    if target % 15 == 0
+        'Fizz Buzz!'
+    elsif target % 5 == 0
+        'Buzz!'
+    elsif target % 3 == 0
+        'Fizz!'
+    else
+        target.to_s
+    end
+end
+
+def fizz_buzz_recursion(target,ans)
+    if target <= 0
+        ans
+    else
+        ans += " " + fizz_buzz(target)
+        target -= 1
+        fizz_buzz_recursion(target, ans)
+    end
+end
+
+result = fizz_buzz_recursion(16, "")
+puts result
